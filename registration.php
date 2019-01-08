280da63a0c453b89379543fa7b1fdc7375364222
Sofiane Bendoukha <bendoukha@dkrz.de>
2019-01-08 10:59:17 +0100

    Adapt input for DKRZ instance.

--- registration.php
+++ registration.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<?php include('server.php') ?>
+<!-- <?php include('server.php') ?> -->
 <html lang="en">
 
   <head>
@@ -10,7 +10,7 @@
     <meta name="author" content="">
 
     <title>ECASLab</title>
-    
+
     <script src="//code.jquery.com/jquery-latest.js" type="text/javascript"></script>
 
     <!-- Bootstrap core CSS -->
@@ -19,13 +19,13 @@
     <!-- Custom styles for this template -->
     <link href="css/small-business.css" rel="stylesheet">
     <link href="css/custom_style.css" rel="stylesheet">
-    
+
         <script type="text/javascript">
 
     	function captcha() {
-    		
+
     		document.getElementById("form").reset();
-    		
+
     	    var alpha = new Array('1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','J','K','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
     	    var i;
     	    for (i=0;i<6;i++){
@@ -37,12 +37,12 @@
     	        var f = alpha[Math.floor(Math.random() * alpha.length)];
     	        var g = alpha[Math.floor(Math.random() * alpha.length)];
     	    }
-    	    
+
     	    var code = a +  b + c + d + e + f + g;
     	    document.getElementById("mainCaptcha").value = code;
-    	    
+
     	    $(this).scrollTop(0);
-    	} 
+    	}
 
     	function refreshCaptcha() {
    		 var alpha = new Array('1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','J','K','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
@@ -56,11 +56,11 @@
    		        var f = alpha[Math.floor(Math.random() * alpha.length)];
    		        var g = alpha[Math.floor(Math.random() * alpha.length)];
    		    }
-   		    
+
    		    var code = a + b + c + d + e + f + g;
-   		    document.getElementById("mainCaptcha").value = code;	
+   		    document.getElementById("mainCaptcha").value = code;
    		}
-    
+
 		$(function() {
 			$('img').click(function() {
 				$('.enlargeImageModalSource').attr('src', $(this).attr('src'));
@@ -71,9 +71,9 @@
 		function refresh() {
 			document.getElementById('first_name').value = "";
 			document.getElementById('last_name').value = "";
-			
+
 		}
-		
+
 	</script>
 
   </head>
@@ -108,9 +108,9 @@
     <br>
       <!-- Heading Row -->
       <div class="row my-4">
-      	<div class="col-lg-2"></div>      	
+      	<div class="col-lg-2"></div>
       	<div class="col-lg-8">
-      	
+
       	<h3 align="center">ECASLab Registration Form</h3>
       	<br>
       	<div class="login-panel panel panel-default">
@@ -139,7 +139,7 @@
                         <div class="form-group">
     				        <textarea class="form-control" style="resize:none" maxlength="300" placeholder="Motivation * (Research, Training, etc.)" id="motivation" rows="5" name="motivation" value="<?php echo $motivation; ?>" autofocus required></textarea>
                         </div>
-                        
+
                         <table class="col-lg-12">
     			        	<tr>
     			           		<td>
@@ -149,12 +149,12 @@
     			          	<tr>
     			           		<td>
     			             		<input class="form-control" type="text" id="mainCaptcha" readonly="readonly" name="mainCaptcha" value = "<?php echo $mainCaptcha; ?>"/>
-    			             		<p>Can't read the above security code? <a href="javascript:void(0);" onclick="refreshCaptcha()">Refresh</a></p>					              		
+    			             		<p>Can't read the above security code? <a href="javascript:void(0);" onclick="refreshCaptcha()">Refresh</a></p>
     			           		</td>
     			          	</tr>
     			          	<tr>
     			          		<td>
-    			            		<input class="form-control" maxlength="15" type="text" id="securityCode" name="securityCode" value = "<?php echo $securityCode; ?>" placeholder="Security Code *" autofocus required/>    
+    			            		<input class="form-control" maxlength="15" type="text" id="securityCode" name="securityCode" value = "<?php echo $securityCode; ?>" placeholder="Security Code *" autofocus required/>
     			          		</td>
     			        	</tr>
     			        	<tr>
@@ -164,21 +164,21 @@
     			          		<td>
     								<label>
     							    	<input class="checkbox-inline" type="checkbox" checked="true" value="yes" id="newsletter" name="newsletter"/>&nbsp;&nbsp;&nbsp;Subscribe to our Mailing List to get the latest news on ECASLab.
-    							    </label>											
+    							    </label>
     			          		</td>
     			        	</tr>
     					</table>
     					<div id="message"></div>
-                        <div class="text-center"> 
+                        <div class="text-center">
                         	<button type="submit" id="submit" name="submit" class="btn btn-primary btn-lg btn-block">Register</button>
                         </div>
                     </fieldset>
                 </form>
             </div>
             <div id="submit"></div>
-            </div>     
+            </div>
         </div>
-        <div class="col-lg-2"></div> 
+        <div class="col-lg-2"></div>
       </div>
       <!-- /.row -->
     </div>
